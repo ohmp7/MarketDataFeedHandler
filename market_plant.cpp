667693e78e0bc212c95059ae912f53390777dab9
Ohm Patel <ohmpatel@Ohms-MacBook-Pro.local>
2025-12-25 22:34:44 -0500

    basic orderbook init

--- market_plant.cpp
+++ market_plant.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <cstdint>
 
+using Price = std::int32_t;
+using Quantity = std::uint32_t;
+
+enum class Side {
+    BUY,
+    SELL,
+};
+
+struct Level {
+    Price level_price;
+    Quantity level_quantity;
+};
+
+// use std::move later
+class OrderBook {
+public:
+    OrderBook(const std::vector<Level>& bids_, const std::vector<Level>& asks_, const std::string& ticker_)
+        : ticker(ticker_), bids(bids_), asks(asks_) {}
+    
+private:
+    std::string ticker;
+    std::vector<Level> bids;
+    std::vector<Level> asks;
+};
+
+int main() {
+
+}
