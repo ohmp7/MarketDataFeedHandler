5768c54bfe50780e0cbe5357515df68cdd0c08ed
Ohm Patel <ohmpatel@Ohms-MBP.fios-router.home>
2025-12-27 18:41:06 -0500

    fix payload def

--- src/market/market_plant.cpp
+++ src/market/market_plant.cpp
@@ -8,30 +8,39 @@ using Quantity = std::uint32_t;
 using InstrumentId = std::uint32_t;
 using Depth = std::size_t;
 using Levels = std::unordered_map<Price, Quantity>;
+using Timestamp = std::uint64_t;
 
 enum class Side : std::uint8_t {
     BID = 0,
-    ASK = 1,
+    ASK = 1
+};
+
+enum class LevelEvent : std::uint8_t {
+    ADD = 0,
+    REDUCE = 1
 };
 
 /* 
 
 Exchange -> Plant API Payload (Network-Byte-Order / Big Endian)
 
-offset 20 - 21:     msg_len         (2 bytes, u16)        ex. 'msg_len' = 18 (bytes)
+offset 20 - 21:     msg_len         (2 bytes, u16)        ex. 'msg_len' = 22 (bytes)
 offset 22 - 25:     instrument_id   (4 bytes, u32)        ex. 'AAPL' = 1
 offset 26:          side            (1 byte, u8)          ex. 'BID' = 0, 'ASK' = 1
 offset 27:          event           (1 byte, u8)          ex. 'ADD' = 0, 'REDUCE' = 1
 offset 28 - 31:     price           (4 bytes, u32)        ex. 'price' = 32 (USD)
-offset 32 - 39:     exchange_ts     (8 bytes, u64)        ex. 1234567891234567890 (ns)
+offset 32 - 35:     quantity        (4 bytes, u32)        ex. 'quantity' = 5917
+offset 36 - 43:     exchange_ts     (8 bytes, u64)        ex. 1234567891234567890 (ns)
 
 */
 
-struct Event {
+struct MsarketEvent {
     InstrumentId instrument_id;
     Side side;
-    Price price;  // if price dne, add a new level
-    Quantity quantity;  // if quantity == 0, erase level
+    LevelEvent event;
+    Price price;
+    Quantity quantity;
+    Timestamp exchange_ts;
 };
 
 // Plant -> Subscriber API (TBD)
